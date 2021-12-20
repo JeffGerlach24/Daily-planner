b17cc84e5283cfffcf43709ecce188a58bd1aea4
Jeff Gerlach <jeff.gerlach2@gmail.com>
2021-12-19 20:29:56 -0800

    JavaScript-functionality

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-const { DateTime } = require("luxon");
+var today = moment().format('LLLL');
+$("#currentDay").html(today);
 
-var DateTime = luxon.DateTime;
+function background () {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+        var divTimeId = parseInt($(this).attr("id"));
+        if (divTimeId < currentHour) {
+            $(this).addClass("past");
+        } else if (divTimeId === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+ }
+ 
+ background ();
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })  
+}); 
+
+$("#00 .description").val(localStorage.getItem("00"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+$("#6 .description").val(localStorage.getItem("6"));
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+$("#22 .description").val(localStorage.getItem("22"));
+$("#23 .description").val(localStorage.getItem("23"));
+
